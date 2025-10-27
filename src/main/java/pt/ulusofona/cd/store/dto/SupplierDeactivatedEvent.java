c2dc9cf9a3e1aa7394248a89eeb1228ecabbf270
nelsonFAlmeida <a22304590@alunos.ulht.pt>
2025-10-27 19:29:42 +0000

    lab 8 Redesign do SupplierDeactivatedEvent para enviar contexto

--- src/main/java/pt/ulusofona/cd/store/dto/SupplierDeactivatedEvent.java
+++ src/main/java/pt/ulusofona/cd/store/dto/SupplierDeactivatedEvent.java
@@ -4,10 +4,18 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.time.LocalDateTime;
+import java.util.UUID;
+
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 public class SupplierDeactivatedEvent {
 
     private String supplierId;
+    private String companyName;
+    private String email;
+    private boolean isActive;
+    private LocalDateTime deactivationDate;
+
 }
